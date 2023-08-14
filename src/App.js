facdf11fb050b417266ec92fd1f69f4070783a42
ldd-8080 <taehee9744@gmail.com>
2023-08-14 15:29:23 +0900

    add styled, img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
+const ImgBox = styled.img`
+  width: 400px;
+  height: 400px;
+`;
+
 function App() {
-  return <div>TEST PAGE LDD</div>;
+  return (
+    <div>
+      <ImgBox
+        src={require("./assets/img/4199672x2_iphone-14-pro__color_black_16004813.png.1000x1000-w.m80.jpg")}
+      />
+    </div>
+  );
 }
 
 export default App;
